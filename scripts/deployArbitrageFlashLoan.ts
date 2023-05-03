4a674064939661f8a9dac332c5b3aaf736e2a0e8
grzeznx2 <grzeznx2@gmail.com>
2023-05-03 10:28:40 +0200

    Add comments

--- scripts/deployArbitrageFlashLoan.ts
+++ scripts/deployArbitrageFlashLoan.ts
@@ -4,6 +4,7 @@ const AAVE_POOL_ADDRESS_PROVIDER = '0x5E52dEc931FFb32f609681B8438A51c675cc232d'
 
 async function main() {
   const AribtrageFlashLoan = await ethers.getContractFactory("AribtrageFlashLoan");
+  // TODO: pass the DEX address
   const afl = await AribtrageFlashLoan.deploy(AAVE_POOL_ADDRESS_PROVIDER);
 
   await afl.deployed();

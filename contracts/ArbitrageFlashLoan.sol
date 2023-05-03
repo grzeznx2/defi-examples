283dffa4c7323f92a69842b796b1be7759416b83
grzeznx2 <grzeznx2@gmail.com>
2023-05-03 10:05:34 +0200

    Scaffold contract methods

--- contracts/ArbitrageFlashLoan.sol
+++ contracts/ArbitrageFlashLoan.sol
@@ -11,4 +11,35 @@ contract ArbitrageFlashLoan is FlashLoanSimpleReceiverBase {
    constructor(address _providerAddress) FlashLoanSimpleReceiverBase(IPoolAddressesProvider(_providerAddress)){
     owner = msg.sender;
    }
+
+   function executeOperation(
+    address asset,
+    uint256 amount,
+    uint256 premium,
+    address initiator,
+    bytes calldata params
+  ) external override returns (bool){
+
+
+    IERC20(asset).approve(address(POOL), amount + premium);
+    return true;
+  }
+
+  function flashLoan(address _token, uint256 _amount) external {
+    POOL.flashLoanSimple(address(this), _token, _amount, '', 0);
+  }
+
+  function getBalance(address _token) external view returns(uint256){
+    return IERC20(_token).balanceOf(address(this));
+  }
+
+  function withdraw(address _token) external onlyOwner {
+    IERC20 token = IERC20(_token);
+    token.transfer(msg.sender, token.balanceOf(address(this)));
+  }
+
+  modifier onlyOwner() {
+    require(msg.sender == owner, "Only for owner");
+    _;
+  }
 }
